54dacb39b8d4de09993162f4df5e7a37f921ecc5
Dilek1979 <dilekatilla79@outlook.com>
2023-10-20 21:25:34 +0200

    login create

--- src/test/java/Pages/Content.java
+++ src/test/java/Pages/Content.java
@@ -1,13 +1,25 @@
 package Pages;
 
 import Utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class Content extends Parent{
+public class Content extends Parent {
 
     public Content() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+
+    @FindBy(css = "input[formcontrolname='username']")
+    public WebElement username;
+    @FindBy(css = "input[formcontrolname='password']")
+    public WebElement password;
+    @FindBy(css = "button[aria-label='LOGIN']")
+    public WebElement loginbuton;
+    @FindBy(css = "span[class='mat-mdc-tooltip-trigger logo-text']")
+    public WebElement textTechnoStudy;
 
 
 }
--- src/test/java/StepDefinitions/logincreate.java
+++ src/test/java/StepDefinitions/logincreate.java
@@ -5,23 +5,29 @@ import Utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.testng.Assert;
 
 public class logincreate {
     Content c = new Content();
 
     @Given("Navigate to Campus")
     public void navigateToCampus() {
-        Driver.getDriver().get("test.mersys.io/");
+        Driver.getDriver().get("https://test.mersys.io/");
 
     }
 
     @When("Enter username and password and click and click clicklogin button")
     public void enterUsernameAndPasswordAndClickAndClickClickloginButton() {
+        c.mySendKeys(c.username,"turkeyts");
+        c.mySendKeys(c.password,"TechnoStudy123");
+        c.myClick(c.loginbuton);
+
 
 
     }
 
     @Then("User should login successfully")
     public void userShouldLoginSuccessfully() {
+        Assert.assertTrue(c.textTechnoStudy.getText().toLowerCase().contains("TechnoStudy"));
     }
 }
