b11047f887dfb92e247176bd29f7f0e25d62ee4a
Dilek1979 <dilekatilla79@outlook.com>
2023-10-22 20:38:50 +0200

    silinecek commit

--- src/test/java/Pages/Content.java
+++ src/test/java/Pages/Content.java
@@ -4,6 +4,7 @@ import Utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class Content extends Parent {
 
@@ -20,14 +21,61 @@ public class Content extends Parent {
     public WebElement loginbuton;
     @FindBy(css = "span[class='mat-mdc-tooltip-trigger logo-text']")
     public WebElement textTechnoStudy;
+    @FindBy(xpath="//ms-add-button[contains(@tooltip,'ADD')]//button")
+    public WebElement addButton;
+    @FindBy(css="span[class='title dialog-title']")
+    public WebElement toolbarName;
 
-    @FindBy(css = "span[class='mat-mdc-button-touch-target")
-    public WebElement Addbutton;
+    @FindBy(xpath="//ms-text-field[@formcontrolname='description']//input")
+    public WebElement descriptionInput;
+    @FindBy(xpath="//ms-text-field[@formcontrolname='code']//input")
+    public WebElement integrationCodeInput;
+    @FindBy(xpath="//ms-integer-field[@formcontrolname='priority']//input")
+    public WebElement priorityInput;
 
-    @FindBy(css = "div[class=mat-form-field-infix ng-tns-c2794762957")
-    public WebElement Nameinput;
-    @FindBy(css = "span[style='visibility:visible;']")
-    public WebElement Savebutton;
+    @FindBy(xpath="//ms-save-button/button")
+    public WebElement saveButton;
+
+    @FindBy(xpath="//div[contains(text(),'successfully')]")
+    public WebElement successMessage;
+
+    @FindBy(xpath = "//input[@data-placeholder='Description']")
+    public WebElement searchDescription;
+    @FindBy(xpath = "//input[@data-placeholder='Integration Code']")
+    public WebElement searchIntegrationCode;
+
+    @FindBy(xpath="//ms-search-button//button")
+    public WebElement searchButton;
+
+    public void staleElement(){
+        wait.until(ExpectedConditions.elementToBeClickable(searchButton));
+    }
+
+    @FindBy(xpath = "//ms-edit-button//button")
+    public WebElement editButton;
+
+    @FindBy(xpath="//ms-delete-button//button")
+    public WebElement deleteButton_1;
+
+    @FindBy(xpath="//button[@type='submit']")
+    public WebElement deleteButton_2;
+
+    @FindBy(xpath="//ms-text-field[@formcontrolname='name']//input")
+    public WebElement nationalityNameInput;
+
+    @FindBy(xpath = "//input[@data-placeholder='Name']")
+    public WebElement searchNationalityName;
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
