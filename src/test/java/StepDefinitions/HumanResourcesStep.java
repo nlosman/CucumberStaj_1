15bc8993cb367a6893a59d217f7a9cfbae85ed8a
HP <osmanenosman@hotmail.com>
2023-10-26 18:40:40 +0200

    Humon res.Positions duzeltildi

--- src/test/java/StepDefinitions/HumanResourcesStep.java
+++ src/test/java/StepDefinitions/HumanResourcesStep.java
@@ -9,56 +9,38 @@ import io.cucumber.java.en.When;
 
 public class HumanResourcesStep {
     Side sd = new Side();
-    Content ct=new Content();
+    Content ct = new Content();
+
+
 
     @Then("Navigate to Human Resources")
     public void navigateToHumanResources() {
-        sd.myClick(sd.HumanResources);
-        sd.myClick(sd.setup);
-        sd.myClick(sd.positions);
+        sd.myClick(sd.humanRecources);
+        sd.myClick(sd.humanRecourcesSetup);
+        sd.myClick(sd.humanRecourcesPositions);
+
     }
 
-    @Given("Click to Human Resources button")
-    public void clickToHumanResourcesButton() {
-        sd.myClick(sd.HumanResources);
-    }
-
-    @When("Click to Setup button")
-    public void clickToSetupButton() {
-        sd.myClick(sd.setup);
-    }
-
-    @Then("Should be open the positions toolbar")
-    public void shouldBeOpenThePositionsToolbar() {
-        sd.myClick(sd.positions);
+    @Then("Should be open the Postion toolbar")
+    public void shouldBeOpenThePostionToolbar() {
+        ct.verifyToolbarOpened(ct.toolbarName, "Position");
     }
 
     @When("Create add New Position  name as {string} and short name as {string}")
     public void createAddNewPositionNameAsAndShortNameAs(String name1, String shortName1) {
-        ct.mySendKeys(ct.name, name1);
-        ct.mySendKeys(ct.shortName, shortName1);
+        ct.mySendKeys(ct.nameInput, name1);
+        ct.mySendKeys(ct.shortNameInput, shortName1);
 
     }
 
-    @And("Delete what is written")
-    public void deleteWhatIsWritten() {
-        ct.myClick(ct.deleteButton_1);
+    @And("Write new name as {string} and click to search button")
+    public void writeNewNameAsAndClickToSearchButton(String name2) {
+        ct.mySendKeys(ct.nameInput_2, name2);
+       ct.myClick(ct.searchButton);
     }
 
-    @And("Write new name as {string} and short name as {string}")
-    public void writeNewNameAsAndShortNameAs(String name2, String shortName2) {
-        ct.mySendKeys(ct.name, name2);
-        ct.mySendKeys(ct.shortName, shortName2);
-
+    @Then("Then  Should be open the Postion toolbar")
+    public void thenShouldBeOpenThePostionToolbar() {
     }
 
-    @When("Click to save button")
-    public void clickToSaveButton() {
-        ct.myClick(ct.saveButton);
-    }
-
-    @Given("Click to delete button in positions")
-    public void clickToDeleteButtonInPositions() {
-        ct.myClick(ct.deleteButton_2);
-    }
 }
